ac31d21fdf427ad99b974e671da500c1701e9130
Collins Kiaritha <thuocollins020@gmail.com>
2025-12-04 06:27:56 +0300

    added the database session to close it

---
+++ lib/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+
+engine = create_engine("sqlite:///betcli.db", echo=False)
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()  # Always close the session to prevent memory leaks
